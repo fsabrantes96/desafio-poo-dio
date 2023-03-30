2ed6fe9bf3eae0d39a412a2d6d209b3a4cc72a29
fsabrantes <fsabrantes96@gmail.com>
2023-03-30 14:22:57 -0300

    Concluindo a criação das classes do domínio

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -9,9 +10,25 @@ public class Dev {
     private Set<Conteudo> conteudoInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootcamp(Bootcamp bootcamp) {}
-    public void progredir() {}
-    public void calcularXp() {}
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+        this.conteudoInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevInscritos().add(this);
+    }
+    public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudoInscritos.stream().findFirst();
+        if(conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudoInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteudo!");
+        }
+    }
+    public double calcularXp() {
+       return this.conteudosConcluidos
+               .stream()
+               .mapToDouble(Conteudo::calcularXp)
+               .sum();
+    }
 
     public String getNome() {
         return nome;
